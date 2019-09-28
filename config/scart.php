e61b1cd396ca6684faa08a1aed36542f7a821b0b
hugginssd <hugginsmafigu@gmail.com>
2019-09-28 15:32:08 +0200

    Initial upload

---
+++ config/scart.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'version' => '3.0.4',
+    'homepage' => 'https://github.com/hugginssd',
+    'name' => 'Sales Analysis',
+    'title' => 'Sales Analysis System',
+    'github' => 'https://github.com/hugginssd/salesanalysis',
+    'email' => 'mafiguhuggins@gmail.com',
+    'settings' => [
+    ],
+];
